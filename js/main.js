4d5bf98f080fc86bf7da9c2ca4832befbc828a78
RosaD97 <r960595@gmail.com>
2023-02-22 16:55:27 +0100

    eventi click da fixare

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use sctrict';
+
+
+// Informazioni Utente
+const nomeCognome = document.getElementById("nomeCognome");
+const chilometri = document.log(getElementById("chilometri").value);
+const fasciaEta = document.getElementById("fasciaEta");
+console.log(chilometri);
+
+// Buttons
+const button = document.querySelector(".chilometri");
+
+button.addEventListener('click',
+    function(){
+        console.log('chilometri'); 
+    }
+
+)
+

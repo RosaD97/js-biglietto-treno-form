7359df31224b94fa0f7d78c11b5265cdba5772f8
RosaD97 <r960595@gmail.com>
2023-02-23 14:02:05 +0100

    fix part1

--- js/main.js
+++ js/main.js
@@ -3,50 +3,67 @@
 
 // Informazioni Utente
 const nomeCognome = document.getElementById("nomeCognome");
-const chilometri = document.getElementById("chilometri").value;
-let fasciaEta = document.getElementById("fasciaEta").value;
+const chilometri = document.getElementById("chilometri");
+const fasciaEta = document.getElementById("fasciaEta");
 
-const minorenne = document.getElementById("minorenne");
-const maggiorenne = document.getElementById("maggiorenne");
-const over65 = document.getElementById("over65");
+// Variabili TIcket
+const textName = document.getElementById('textnomeCognome');
+const textOffer = document.getElementById('ticket');
+const carrozza = document.getElementById('carrozza');
+const textCp = document.getElementById('text-cp');
+const textPrezzo = document.getElementById('prezzo');
 
-// Prezzo del biglietto al km
-const prezzoAlKm = 0.21;
 
-// Buttons
+// Button
 const button = document.querySelector(".button");
+const reset = document.getElementById('reset');
 
-// Prezzo del biglietto finale
-let prezzoBiglietto = chilometri * prezzoAlKm;
+const display = document.querySelector(".display");
 
-// Prezzo finale con sconti applicati
-if (fasciaEta == minorenne){
-    prezzoBiglietto - (prezzoBiglietto / 100 * 20);
-} else if (fasciaEta == over65) {
-    prezzoBiglietto - (prezzoBiglietto / 100 * 40);
-}
 
 // Evento click su bottone Genera
 button.addEventListener('click',
     function(){ 
-        let mostra = document.querySelector(".display");
-        mostra.classList.toggle("display");
-                // Dati Nome e Cognome stampati in ticket
-                const utente = nomeCognome.value;
-                document.querySelector(".nomeCognome").innerHTML = utente;
-                // Prezzo biglietto stampato in ticket
-                const prezzoFinale = (prezzoBiglietto) + `€`; 
-                document.querySelector(".prezzo").innerHTML = prezzoFinale;
-    }
-)
+        // Valori richiesti
+        const inputNameValue = nomeCognome.value;
+        const inputKmValue = Number(chilometri.value);
+        const inputEtaValue = fasciaEta.value;
+
+        console.log({inputNameValue, inputKmValue, inputEtaValue});
+
+        let prezzoBiglietto = inputKmValue * 0.21;
+        let message = 'Biglietto Standard';
+        // Prezzo finale con sconti applicati
+            if (inputEtaValue === 'minorenne'){
+                prezzoBiglietto = prezzoBiglietto * 0.8;
+                message = 'Sconto Minorenne';
+            } else if (inputEtaValue === 'over65') {
+                prezzoBiglietto = prezzoBiglietto * 0.6;
+                message = 'Sconto Over';
+            }
+
+        prezzoBiglietto = prezzoBiglietto.toFixed(2);
+        console.log(prezzoBiglietto);
+
+        textName.innerHTML = inputNameValue;
+        textOffer.innerHTML = message;
+        carrozza.innerHTML = nomeCognome;
+        textCp.innerHTML = nomeCognome;
+        textPrezzo.innerHTML =  `${prezzoBiglietto} $ `;
+
+        // Mostra biglietto
+        display.classList.add("displayshow");
+
+        
 
 
-const reset = document.querySelector(".reset");
+        }
+);
 
-button.addEventListener('click',
-    function clearResult(form){
-        document.getElementById("nomeCognome").value;
-    }
-)
+reset.addEventListener('click', 
+function(){
+    display.classList.remove("display");
+    textName.value = '';
 
 
+})
